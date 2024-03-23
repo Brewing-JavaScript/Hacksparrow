6c92649eeee8fd50b3b458ed0029c9e492198949
Farhaan <farhaan8d@gmail.com>
2024-03-23 07:32:37 +0530

    Fixed bg Color

--- src/Home/NewsDetails.jsx
+++ src/Home/NewsDetails.jsx
@@ -1,40 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useContext } from 'react';
+import api from '../Api/Api';
+import { useParams } from 'react-router-dom';
+import { UrlContext } from '../App';
 
 const ArticleDetail = () => {
     const [article, setArticle] = useState(null);
+    let { currentUrl, setCurrentUrl } = useContext(UrlContext)
 
-    const summarizeContent = async (content) => {
-        try {
-            const response = await axios.post('http://localhost:5000/summarize', { content });
-            console.log('Summary:', response.data.summary);
-        } catch (error) {
-            console.error('Error summarizing content:', error);
-        }
-    };
 
     useEffect(() => {
-        // Fetch article details
-        // Code for fetching article details goes here
-    }, []);
+        console.log(currentUrl);
+        const fetchArticle = async () => {
+            try {
+                if (currentUrl) {
+                    const response = await api.post('/detail-news', { currentUrl });
+                    setArticle(response.data.article);
+                }
+            } catch (error) {
+                console.error('Error fetching article:', error);
+            }
+        };
+
+        fetchArticle();
+    }, [currentUrl]); // Dependency array with currentUrl
 
     if (!article) {
         return <div>Loading...</div>;
     }
-
     return (
+
         <div className="article-container">
             <div className="news-page">
                 <h1>Article Details</h1>
-                <div>
-                    <h2>{article.title}</h2>
-                    <p>{article.byline}</p>
-                    <p>Published Time: {new Date(article.publishedTime).toLocaleString()}</p>
-                    <img src={article.image} alt="Article" />
-                    <div dangerouslySetInnerHTML={{ __html: article.content }}></div>
-                    <p>Length: {article.length} words</p>
-                    <button onClick={() => summarizeContent(article.content)}>Summarize Content</button>
-                </div>
+                {article ? (
+                    <div>
+                        <h2>{article.title}</h2>
+                        <p>{article.byline}</p>
+                        <p>Published Time: {new Date(article.publishedTime).toLocaleString()}</p>
+                        <img src={article.image} alt="Article" />
+                        <div dangerouslySetInnerHTML={{ __html: article.content }}></div>
+                        <p>Length: {article.length} words</p>
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
             </div>
         </div>
     );
